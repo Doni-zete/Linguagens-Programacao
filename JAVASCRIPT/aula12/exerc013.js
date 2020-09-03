3524bbd11bdd885173f9831104ec9c6964d8e096
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-03 15:42:12 -0300

    javascript

--- JAVASCRIPT/aula12/exerc013.js
+++ JAVASCRIPT/aula12/exerc013.js
@@ -2,7 +2,8 @@ var agora = new Date()
 var diaSem = agora.getDay()
 
 
-console.log(diaSem)
+diaSem= 10
+
 switch (diaSem) {
     case 0:
         console.log('Domingo')
@@ -19,11 +20,14 @@ switch (diaSem) {
         break
     case 4:
         console.log('Quinta')
+        break
     case 5:
         console.log('Sexta')
         break
     case 6:
         console.log('Sabado')
         break
+        default:
+            console.log('ERRO dia invalido')
 
 }
---
+++ JAVASCRIPT/aula12/pratic014.js
@@ -0,0 +1,20 @@
+var moeda 
+
+moeda = 2
+switch (moeda) {
+    case 1:
+        console.log('ether')
+        break;
+        case 2:
+        console.log('btc')
+        break;
+        case 3:
+        console.log('vsc')
+        break;
+        case 4:
+        console.log('ltc')
+        break;
+
+    default:
+        break;
+}
